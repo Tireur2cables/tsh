9ec5d7f26c83d06d08a9c536cf77195ef7ba0aab
Adam Jami <Seijuro>
2020-10-06 02:51:18 +0200

    ajout fichier initial

---
+++ code/cd.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "tar.h"
+
+
+int main(void) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
