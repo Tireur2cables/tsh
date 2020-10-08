003d12a896050d9fd83595b0f33c0e900994e8c2
BARRAULT Victor <victor.barrault@etu.univ-paris-diderot.fr>
2020-10-08 18:59:40 +0200

    ajout de is_tar

--- code/ls.c
+++ code/ls.c
@@ -15,16 +15,17 @@ void show_complete_header_infos(struct posix_header *, int *);
 int print_normal_dir(DIR*);
 int print_inoeud_normal_dir(DIR*);
 int print_dir(char *, char *);
+int is_tar(char *);
 //fixme implémenter
 int check_options(char *);
 
 //FONCTION LS
 /*TODO : FONCTION CHECK_OPTIONS
-		 FONCTION is_tar et supprimé options -t
 		 REMPLACER TOUT LES PRINTF PAR DES WRITE
 		 GERER LES OPTIONS (-a dans ., dans
 		 TAR : - Afficher les droits correctement - nombre de references - createur - date
 		 	   - Changer le read inutile en lseek
+		FIXME : ERREUR DE SEGMENTATION
 
 */
 
@@ -47,74 +48,77 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-int print_dir(char *file /*NULL si repetoire = .*/, char *options/*NULL si pas d'options*/){
-	if(strcmp(file, ".") == 0){ //repertoire courant
-		//fixme gerer les options
-		/*
-		if(option == x){
-		}else{
-		}*/
-		DIR* dir = opendir(file);
-		print_normal_dir(dir);
-		closedir(dir);
-
-	}else{ //repetoire en paramètre
-		//fixme gerer les options
-		/*if(is_tar(file)){
-			if(option == x){
-			}else{
+int print_dir(char *file, char *options){
+	char *cp = malloc(sizeof(file)+1);
+	strcpy(cp, file);
+	if(is_tar(cp) == 0){
+		if(strcmp(options, "\0") == 0){ //pas d'option
+			struct posix_header * header = malloc(sizeof(struct posix_header));
+			assert(header);
+			int fd = open(file, O_RDONLY);
+			if(fd == -1){
+			  perror("erreur d'ouverture du fichier");
+			  return -1;
 			}
+
+			int n = 0;
+			int read_size = 0;
+			while((n=read(fd, header, BLOCKSIZE))>0){
+				if(strcmp(header->name, "\0") == 0){
+					return 0;
+				}
+				show_simple_header_infos(header, &read_size);
+				/*TODO : Changer en lseek*/
+				read(fd, header, BLOCKSIZE*read_size);
+			}
+			printf("\n");
+			close(fd);
 		}else{
-			if(option == x){
-			}else{
+			struct posix_header * header = malloc(sizeof(struct posix_header));
+			assert(header);
+
+			int fd = open(file, O_RDONLY);
+			if(fd == -1){
+			  perror("erreur d'ouverture du fichier");
+			  return -1;
+			}
+
+			int n = 0;
+			int read_size = 0;
+			while((n=read(fd, header, BLOCKSIZE))>0){
+				if(strcmp(header->name, "\0") == 0){
+					return 0;
+				}
+				show_complete_header_infos(header, &read_size);
+				read(fd, header, BLOCKSIZE*read_size);
+			}
+			printf("\n");
+			close(fd);
 		}
-		}*/
+	}else{
 		if(strcmp(options, "\0") == 0){ //pas d'options
-			char *arg = ".";
-			DIR* dir = opendir(arg);
+			DIR* dir = opendir(file);
 			print_normal_dir(dir);
 			closedir(dir);
 		}else{
-			//fixme fonctions istar
-			if(strcmp(options, "\t")){ //temporaire pour gérer les tars a remplacer par une fonction is_tar(char *file)
-				struct posix_header * header = malloc(sizeof(struct posix_header));
-				assert(header);
-
-				int fd = open(file, O_RDONLY);
-				if(fd == -1){
-			      perror("erreur d'ouverture du fichier");
-				  return -1;
-			    }
-
-				int n = 0;
-				int read_size = 0;
-				while((n=read(fd, header, BLOCKSIZE))>0){
-					if(strcmp(header->name, "\0") == 0){
-						return 0;
-					}
-					show_complete_header_infos(header, &read_size);
-					/*if((strcmp(options, "\0") == 0)){
-						show_simple_header_infos(header, &read_size);
-					}
-					else if((strcmp(options, "-l") == 0)){
-						show_complete_header_infos(header, &read_size);
-					}*/
-					/*TODO : Changer en lseek*/
-					read(fd, header, BLOCKSIZE*read_size);
-				}
-				printf("\n");
-				close(fd);
-			}
-			else{
-				char *arg = ".";
-				DIR* dir = opendir(arg);
-				print_inoeud_normal_dir(dir);
-				closedir(dir);
-			}
+			DIR* dir = opendir(file);
+			print_inoeud_normal_dir(dir);
+			closedir(dir);
 		}
 	}
 	printf("\n");
 	return 0;
+}
+
+int is_tar(char *file){
+	char *token, *last;
+	last = token = strtok(file, ".");
+	while(token != NULL){
+		last = token;
+		token = strtok(NULL, ".");
+	}
+	if(last == NULL) return -1;
+	return strcmp(last, "tar");
 }
 
 void show_complete_header_infos(struct posix_header *header, int *read_size){
