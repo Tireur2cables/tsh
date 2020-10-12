76a2af4e2c21d75bcb3e8438de59075e49e8330c
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-10-12 22:01:45 +0200

    Implémentation readline
    Problem : implémentation de la librairie readline pour gérer n'importe 
    quelle taille de commande
    
    Solution : voir readme pour modifications engendrées par readline
    
    Note :

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Projet_Systeme
 
+`sudo apt-get install libreadline-dev` permet d'installer la librairie readline  
+`-lreadline` dans le makefile pour le linkeur  
--- code/tsh.c
+++ code/tsh.c
@@ -8,17 +8,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <readline/readline.h>
 #include "ls.h"
 
-#define BUFSIZE 512
-
-char mycat_buf[BUFSIZE];
+char *pwd = ".";
 
 int iscmd(char *, char *);
 int isOnlySpace(char *, int);
 void selectCommand(int, char *);
-
-char *pwd = ".";
 
 int main(int argc, char const *argv[]) {
 	if (argc > 1) {
@@ -30,18 +27,28 @@ int main(int argc, char const *argv[]) {
 	char *prompt = "$ ";
 	int prompt_len = strlen(prompt);
 	while (1) {
-		if (write(STDOUT_FILENO, prompt, prompt_len) < prompt_len) {
-			perror("Erreur d'écriture du prompt!");
-			exit(EXIT_FAILURE);
-		}
+		char new_prompt[strlen(pwd) + 1 + prompt_len + 1];
+		strcpy(new_prompt, pwd);
+		new_prompt[strlen(pwd)] = ' ';
+		new_prompt[strlen(pwd)+1] = '\0';
+		strcat(new_prompt, prompt);
+
 		int readen;
-		if ((readen = read(STDIN_FILENO, mycat_buf, BUFSIZE)) < 0) {
-			perror("Erreur de lecture dans le shell!");
-			exit(EXIT_FAILURE);
+		char *mycat_buf;
+		if ((mycat_buf = readline (new_prompt)) != NULL) {
+			readen = strlen(mycat_buf);
+			if (readen > 0 && !isOnlySpace(mycat_buf, readen)) {
+				selectCommand(readen, mycat_buf);
+			}
+		}else {
+			char *newline = "\n";
+			int newline_len = strlen(newline);
+			if (write(STDOUT_FILENO, newline, newline_len) < newline_len) {
+				perror("Erreur d'écriture dans le shell!");
+				exit(EXIT_FAILURE);
+			}
 		}
-		if (readen > 1 && !isOnlySpace(mycat_buf, readen)) {
-			selectCommand(readen, mycat_buf);
-		}
+		free(mycat_buf); // FIXME : les exit font-ils free
 	}
 
 	return 0;
@@ -58,16 +65,16 @@ void selectCommand(int readen, char *mycat_buf) {
 		exit(EXIT_SUCCESS);
 	}else if (iscmd(mycat_buf, "ls")) {
 		int status;
-		char mycat_buf_copy[readen+1];
 		int pid = fork();
 	    switch(pid) {
-	    	case -1:
+	    	case -1: {//erreur
 				perror("Erreur de fork!");
 	     		exit(EXIT_FAILURE);
-	    	case 0: //fils
+			}
+	    	case 0: {//fils FIXME: c'est ls qui devrait gérer les différents cas? ou alors dans une fonction différente ?
+				char mycat_buf_copy[readen+1];
 				strncpy(mycat_buf_copy, mycat_buf, readen);
 				mycat_buf_copy[readen] = '\0';
-				strtok(mycat_buf, "\n");
 				strtok(mycat_buf, " ");
 				if (strtok(NULL, " ") == NULL) {
 					char *argv2[2];
@@ -75,16 +82,14 @@ void selectCommand(int readen, char *mycat_buf) {
 					argv2[1]=pwd;
 					ls(1, argv2);
 				}else {
-					char *args;
-					strtok(mycat_buf_copy, " ");
-					args = strtok(NULL, "\n");
 					char *argv2[2];
 					argv2[0]="ls";
-					argv2[1]=args;
+					argv2[1]=&mycat_buf_copy[3]; //cmd without "ls "
 					ls(2, argv2);
 				}
 				exit(EXIT_SUCCESS);
-	    	default: //pere
+	    	}
+			default: {//pere
 				if (waitpid(pid, &status, 0) < 0) {
 					perror("Erreur de wait!");
 					exit(EXIT_FAILURE);
@@ -97,6 +102,7 @@ void selectCommand(int readen, char *mycat_buf) {
 						exit(EXIT_FAILURE);
 					}
 				}
+			}
 	    }
 	}else if (iscmd(mycat_buf, "help")) {
 		char *help = "Voici une liste non exhaustive des commandes implémentées:ǹ\nexit : quitte le tsh\nls : wip\nhelp : obtenir la liste des commandes\n\n";
@@ -124,5 +130,5 @@ int isOnlySpace(char *mycat_buf, int readen) {
 
 
 int iscmd(char *mycat_buf, char *cmd) {
-	return (strncmp(mycat_buf, cmd, strlen(cmd)) == 0) && (isspace(mycat_buf[strlen(cmd)]));
+	return (strncmp(mycat_buf, cmd, strlen(cmd)) == 0) && ((isspace(mycat_buf[strlen(cmd)])) || (mycat_buf[strlen(cmd)] == '\0'));
 }
