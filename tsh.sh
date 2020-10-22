62b2198052b7903c5eb02d0dc2043a7c3860c64b
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-10-22 16:31:44 +0200

    amélioration tsh.sh
    Problem : amélioration du script de lancement pour prendre en compte le 
    cas ou le programme n'est pas compilé
    
    Solution : 
    
    Note :

--- tsh.sh
+++ tsh.sh
@@ -2,4 +2,11 @@
 
 #Lancement du programme tsh
 
-./code/tsh
+# Vérification de l'existence de l'executable issu de la compilation par make
+if [ -f code/tsh ]
+then # Lance le programme
+	./code/tsh
+else # Compile le programme avant de le lancer
+	make -C code/
+	./code/tsh
+fi
