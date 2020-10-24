8f05451163b4fe7a16327e1d0d2313333bdd8ea7
BARRAULT Victor <victor.barrault@etu.univ-paris-diderot.fr>
2020-10-24 17:15:22 +0200

    début de la redaction des testsé

--- code/test.c
+++ code/test.c
@@ -19,11 +19,16 @@ int main(int argc, char const *argv[]) {
 	int output = open("../test/test_out",  O_WRONLY + O_CREAT + O_TRUNC, S_IRWXU);
 	int input = open("../test/test_in",  O_RDONLY);
 	char in_str[71];
+<<<<<<< HEAD
 	char out_str[71];
+=======
+	char out_str[4];
+>>>>>>> début de la redaction des testsé
 	//write(output, "test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n", strlen("test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n"));
 	//printf("o : %d, i : %d ", output, input);
 	int save = dup(1);
 	//printf("%d", save_out);
+<<<<<<< HEAD
 	dup2(output, STDOUT_FILENO);
 	char *arg[2];
 	arg[0] = "ls";
@@ -38,6 +43,24 @@ int main(int argc, char const *argv[]) {
 	read(input, in_str, 70);
 	printf(out_str);
 	printf(in_str);
+=======
+	printf("%d", dup2(output, STDOUT_FILENO));
+	printf("aled");
+	/*
+	char *arg[2];
+	arg[0] = "ls";
+	arg[1] = "tests";
+	ls(2,arg);*/
+	close(output);
+	printf("%d", dup2(STDOUT_FILENO, save));
+	//close(save_out);
+
+	int output2 = open("../test/test_out",  O_RDONLY);
+	read(output2, out_str, 4);
+	//read(input, in_str, 70);
+	printf(out_str);
+	//printf(in_str);
+>>>>>>> début de la redaction des testsé
 	if(strcmp(out_str, in_str) == 0){
 		printf("Les fichiers sont les mêmes");
 	}else{
