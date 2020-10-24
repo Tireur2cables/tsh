2e19f20ea4b4ace6c3aa5b0ff4111a2ee2c3efae
BARRAULT Victor <victor.barrault@etu.univ-paris-diderot.fr>
2020-10-24 17:21:33 +0200

    avancement du test de fonctionnement de ls

--- code/test.c
+++ code/test.c
@@ -19,27 +19,25 @@ int main(int argc, char const *argv[]) {
 	int output = open("../test/test_out",  O_WRONLY + O_CREAT + O_TRUNC, S_IRWXU);
 	int input = open("../test/test_in",  O_RDONLY);
 	char in_str[71];
-	char out_str[4];
+	char out_str[71];
 	//write(output, "test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n", strlen("test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n"));
 	//printf("o : %d, i : %d ", output, input);
 	int save = dup(1);
 	//printf("%d", save_out);
-	printf("%d", dup2(output, STDOUT_FILENO));
-	printf("aled");
-	/*
+	dup2(output, STDOUT_FILENO);
 	char *arg[2];
 	arg[0] = "ls";
 	arg[1] = "tests";
-	ls(2,arg);*/
+	ls(2,arg);
 	close(output);
-	printf("%d", dup2(STDOUT_FILENO, save));
-	//close(save_out);
+	dup2(save, STDOUT_FILENO);
+	close(save);
 
 	int output2 = open("../test/test_out",  O_RDONLY);
-	read(output2, out_str, 4);
-	//read(input, in_str, 70);
+	read(output2, out_str, 70);
+	read(input, in_str, 70);
 	printf(out_str);
-	//printf(in_str);
+	printf(in_str);
 	if(strcmp(out_str, in_str) == 0){
 		printf("Les fichiers sont les mêmes");
 	}else{
