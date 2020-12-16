5839987b8644fd24b06fae1891879496085d18c5
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-12-16 19:34:44 +0100

    Modification de cd.h
    Problem : 
    
    Solution : 
    
    Note :

--- code/cd.h
+++ code/cd.h
@@ -1,19 +1,6 @@
 #ifndef CD_H
 #define CD_H
 
-
 extern int cd(int, char *[]);
-
-int parcoursChemin(char *, char *, char *);
-int parcoursTar(char *, char *, char *);
-int isAccessibleFrom(char *, char *);
-int setPath(char *, char *);
-int errorDetect(int);
-int isTar(char *);
-int isSameDir(char *, char *);
-int findTarIn(char const *, int);
-int appelSurTar(char *, char *);
-int getLenLast(char const *);
-char *getRealChemin(char *, char *);
 
 #endif
