8f7d6c31d47351c5fe2ccc8f401a7077bf3ca082
BARRAULT Victor <victor.barrault@etu.univ-paris-diderot.fr>
2020-12-24 12:04:56 +0100

    avancement mkdiré

--- code/mkdir.c
+++ code/mkdir.c
@@ -37,12 +37,20 @@ int mkdir_tar(int argc, char *argv[]) {
 	else{
 		for(int i = 1; i < argc; i++){
 			if(getenv("TWD") != NULL){
+				write(STDOUT_FILENO, getenv("TWD"), strlen(getenv("TWD")));
+				write(STDOUT_FILENO, "\n", 1);
 				if(is_tar_mk(getenv("TWD"))){
 					if(argv[i][0] == '/'){ //Si l'appel ressort du tar (avec .. ou ~ par exemple), alors l'argument est transformé en chemin partant de la racine
 						try_create_dir(argv[i]);
 					}else{
+<<<<<<< HEAD
 						char file[strlen(getenv("TWD")) + strlen(argv[i])];
 						sprintf(file, "%s/%s", getenv("TWD"), argv[i]);
+=======
+						char file[strlen(getenv("TWD")) + strlen(argv[1])];
+						sprintf(file, "%s/%s", getenv("TWD"), argv[1]);
+						write(STDOUT_FILENO, file, strlen(file));
+>>>>>>> avancement mkdiré
 						try_create_dir(file);
 					}
 				}else{
@@ -152,8 +160,13 @@ int exist_file(char *name){
 }
 
 int exist_dir(char *namefile, char *tarfile){
+<<<<<<< HEAD
 	//write(STDOUT_FILENO, "\n",1);
 	//write(STDOUT_FILENO, tarfile, strlen(tarfile));
+=======
+	write(STDOUT_FILENO, "\n",1);
+	write(STDOUT_FILENO, tarfile, strlen(tarfile));
+>>>>>>> avancement mkdiré
 	struct posix_header header;
 	int fd = open(tarfile, O_RDONLY);
 	if(fd == -1){
