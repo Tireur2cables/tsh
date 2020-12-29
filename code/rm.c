4bbdfa21e3ef59dd88bde9f5ee6b7d9846ff89fa
Adam Jami <Seijuro>
2020-12-30 00:08:42 +0100

    rm fonctionnel

--- code/rm.c
+++ code/rm.c
@@ -13,19 +13,18 @@
 #include "tar.h"
 #include "rm.h"
 
-int parcoursCheminTar_rm(char * , char * , char * , int );
+int parcoursCheminTar_rm(char * , char * , char *,int);
 int parcoursChemin_rm(char * , char * , int );
+int delete_dir_tar(char * , char * , char *);
+int delete_file_tar(char * , char * , char *);
 int detectError(int );
 int isSameDir_rm(char *, char *);
 int isAccessibleFrom_rm(char * , char * , int);
 int is_ext_rm(char *, char *);
 int is_tar_rm(char *);
-int dir_or_file(char * , char * , char * , int );
-int delete_dir(char *);
-int delete_tar(char * , char * , char * , int);
 
 
-int main(int argc , char ** argv) {
+int rm_func(int argc , char ** argv) {
 	
 	if (detectError(argc)) return -1;
 	
@@ -119,7 +118,7 @@ int parcoursChemin_rm(char * chemin, char * pwd , int option) {
 	char resdoss[strlen(cpydoss+1)];
 	strcpy(resdoss,cpydoss);
 
-	dir_or_file(resdoss,oldpwd,res,option);
+	
 	
 	free(cpydoss);
 	free(oldpwd);
@@ -184,7 +183,7 @@ int parcoursCheminTar_rm(char * doss , char * oldpwd , char * rest , int option)
 	strcpy(absolutetar, oldpwd);
 	strcat(absolutetar, "/");
 	strcat(absolutetar, tar);
-
+	
 	char chemin[strlen(doss) - strlen(tar) + strlen(rest) + 1];
 	if (strlen(doss) - strlen(tar) > 0) strcpy(chemin, &doss[strlen(tar)+1]);
 	else strcpy(chemin, &doss[strlen(tar)]);
@@ -227,6 +226,13 @@ int parcoursCheminTar_rm(char * doss , char * oldpwd , char * rest , int option)
 		}
 	}else found = 1;
 
+	char res[strlen(absolutetar) + 1 + strlen(chemin) + 1];
+		strcpy(res, absolutetar);
+		if (chemin != NULL && strlen(chemin) != 0) {
+			strcat(res, "/");
+			strcat(res, chemin);
+		}
+	
 	close(fd);
 	if (!found) {
 		char *deb  = "rm : ";
@@ -240,171 +246,192 @@ int parcoursCheminTar_rm(char * doss , char * oldpwd , char * rest , int option)
 		int errorlen = strlen(error);
 		if (write(STDERR_FILENO, error, errorlen) < errorlen)
 			perror("Erreur d'écriture dans le shell!");
-	} else {
-		char res[strlen(absolutetar) + 1 + strlen(chemin) + 1];
-		strcpy(res, absolutetar);
-		if (chemin != NULL && strlen(chemin) != 0) {
-			strcat(res, "/");
-			strcat(res, chemin);
-		}
-		if (res[strlen(res)-1] == '/') res[strlen(res)-1] = '\0';
-		
-		//printf("%s\n",absolutetar); // chemin avec tar
-		//printf("%s\n",chemin); // après le tar
-		//printf("%s\n",res);  // concat
+	} else if (found == 1) {
+
+		if(strcmp(absolutetar,res)==0) {   // delete entire tar
 	
-		if((int)strlen(chemin)==0 ) {  // si rien après tar
+		if(option<=0) {
 			
-			if(doss[strlen(doss)-1]=='/') {} //  cmt savoir avec strtok ?
-		
-			printf("suppression du tar vide...\n");
-			/*if(remove(res)!=0) {
+			char *deb  = "rm : ";
+			char *end = " est un répertoire !\n";
+			char error[strlen(deb) + strlen(res) + strlen(end) + 1];
+			strcpy(error, deb);
+			strcat(error, res);
+			strcat(error, end);
+			int errorlen = strlen(error);
+			if (write(STDERR_FILENO, error, errorlen) < errorlen)
+				perror("Erreur d'écriture dans le shell!");
+			return -1;
+			
+			} 
+			
+			if(remove(res)!=0) {
 				perror("erreur de supression du fichier");
 				return -1;
-				}*/
-			} else {
+				}
 				
-				delete_tar(absolutetar,chemin,res,option);
-				
-			}
-	
-	}
-	
-	return (found == 1)? 0 : -1;
-
-}
-
-
-int delete_tar(char * absolutetar , char * chemin , char * res , int option) {
-	
-	
-	
-	
-	
-	
-	return 0;
-}
-
-int dir_or_file(char * doss , char * oldpwd , char * concat , int option ) {
-	
-	//printf("%s\n",doss);
-	//printf("%s\n",oldpwd);
-	//printf("%s\n",concat);
-	
-	DIR * dir = opendir(oldpwd);
-	
-	if (dir == NULL) {
-		char *error_debut = "rm : Erreur! Impossible d'ouvrir le répertoire ";
-		char error[strlen(error_debut) + 1];
-		strcpy(error, error_debut);
-		strcat(error, "\n");
-		int errorlen = strlen(error);
-		if (write(STDERR_FILENO, error, errorlen) < errorlen)
-			perror("Erreur d'écriture dans le shell!");
-		return -1;
-	}
-	
-	struct dirent * d;
-	struct stat st;
-	
-	while((d=readdir(dir))!=NULL) {
-		
-		if(strcmp(d->d_name,doss)==0) {
+		} else {  //delete dir in tar
 			
-		if (stat(concat, &st) < 0) {
-			perror("Erreur de stat!");
-			return -1;
-		}
-		
-		if(S_ISDIR(st.st_mode) && option>0) {
-			
-			printf("suppression du répertoire...\n");
-			//delete_dir(concat);
-			
-		} else {
-			if(S_ISDIR(st.st_mode) && option<=0) {
-				
+			if(option<=0) {
 				char *deb  = "rm : ";
 				char *end = " est un répertoire !\n";
-				char error[strlen(deb) + strlen(doss) + strlen(end) + 1];
+				char error[strlen(deb) + strlen(res) + strlen(end) + 1];
 				strcpy(error, deb);
-				strcat(error, doss);
+				strcat(error, res);
 				strcat(error, end);
 				int errorlen = strlen(error);
 				if (write(STDERR_FILENO, error, errorlen) < errorlen)
 					perror("Erreur d'écriture dans le shell!");
 				return -1;
-				
 			} else {
-			
-				printf("suppression du fichier...\n");
-				/* if(remove(concat)!=0) {
-					perror("erreur de suppression du fichier");
-					return -1;
-					} */
-			
-				}
-		
+			delete_dir_tar(absolutetar,chemin,res);	
 			}
-		
+			
 		}
+
+
+	}else { //delete file in tar
+		
+		delete_file_tar(absolutetar,chemin,res);
 		
 	}
 	
-	return 0;
 	
+	
+	return (found == 1)? 0 : -1;
+
 }
 
-int delete_dir(char * pwd) {
+int delete_dir_tar(char * absolutetar , char * chemin , char * res ) {
 	
-	DIR * dir = opendir(pwd);
+	int fd = open(absolutetar,O_RDWR);
+	struct posix_header * header = malloc(sizeof(struct posix_header));
 	
-	if (dir == NULL) {
-		char *error_debut = "rm : Erreur! Impossible d'ouvrir le répertoire ";
-		char error[strlen(error_debut) + 1];
+	if (fd == -1) {
+		char *error_debut = "rmdir : Erreur! Impossible d'ouvrir l'archive ";
+		char error[strlen(error_debut) + strlen(absolutetar) + 1 + 1];
 		strcpy(error, error_debut);
+		strcat(error, absolutetar);
 		strcat(error, "\n");
 		int errorlen = strlen(error);
 		if (write(STDERR_FILENO, error, errorlen) < errorlen)
 			perror("Erreur d'écriture dans le shell!");
-		return -1;
 	}
 	
-	struct dirent * d;
-	struct stat st;
+	int found=0;
 	
-	while((d=readdir(dir))!=NULL) {
-	
-	
-	 if (strcmp(d->d_name, ".") == 0 || strcmp(d->d_name, "..") == 0) continue;
-	 	 
-	  char newpwd[strlen(pwd)+1+strlen(d->d_name)+1+1];
-	  strcpy(newpwd,pwd);
-	  strcat(newpwd,"/");
-	  strcat(newpwd,d->d_name);
+	while(!found) {
 		
-	 if (stat(newpwd, &st) < 0) {
-			perror("Erreur de stat!");
-			return -1;
+		if(read(fd,header,BLOCKSIZE)<BLOCKSIZE) break;
+		
+		char nom[strlen(header->name)+1];
+		strcpy(nom,header->name);
+		
+		if(strcmp(nom,"\0")==0) break;
+		
+		int taille = 0;
+		int *ptaille = &taille;
+		sscanf(header->size, "%o", ptaille);
+		int filesize = ((*ptaille + 512-1)/512);
+
+	
+		
+		if(strncmp(nom, chemin, strlen(chemin)) == 0 ) {
+			
+			off_t position;
+			off_t endposition;
+
+			if((position=lseek(fd, -BLOCKSIZE , SEEK_CUR))==-1) break;
+			if((endposition=lseek(fd, 0 , SEEK_END))==-1) break;
+
+			unsigned int size= endposition - (position+BLOCKSIZE+filesize*BLOCKSIZE);
+			char cpy[size];
+
+			if(lseek(fd, position+BLOCKSIZE+filesize*BLOCKSIZE , SEEK_SET)==-1) break;
+			read(fd , cpy , size);
+
+			if(lseek(fd, position , SEEK_SET)==-1) break;
+			write(fd , cpy , size);	
+			
+			if(lseek(fd,position,SEEK_SET)==-1) break;
+			
+		} else {
+		
+		read(fd,header,BLOCKSIZE*filesize);
+		
 		}
 	
-	 if(S_ISDIR(st.st_mode)) {
-            delete_dir(newpwd);
-        } else {
-           if(remove(newpwd)!=0) {
-			perror("erreur de supression du fichier");
-			return -1;
-		   }
-		}
-		
 	}
 	
-	closedir(dir);
+	
+	close(fd);
+
+	
+	return 0;
+}
+
+int delete_file_tar(char * absolutetar , char * chemin , char * res ) {
+	
+	int fd = open(absolutetar,O_RDWR);
+	struct posix_header * header = malloc(sizeof(struct posix_header));
+	
+	if (fd == -1) {
+		char *error_debut = "rmdir : Erreur! Impossible d'ouvrir l'archive ";
+		char error[strlen(error_debut) + strlen(absolutetar) + 1 + 1];
+		strcpy(error, error_debut);
+		strcat(error, absolutetar);
+		strcat(error, "\n");
+		int errorlen = strlen(error);
+		if (write(STDERR_FILENO, error, errorlen) < errorlen)
+			perror("Erreur d'écriture dans le shell!");
+	}
+	
+	int found=0;
+	
+	while(!found) {
 		
-		if(rmdir(pwd)!=0) {
-			perror("erreur de suppression du répertoire");
-			return -1;
+		if(read(fd,header,BLOCKSIZE)<BLOCKSIZE) break;
+		
+		char nom[strlen(header->name)+1];
+		strcpy(nom,header->name);
+		
+		int taille = 0;
+		int *ptaille = &taille;
+		sscanf(header->size, "%o", ptaille);
+		int filesize = ((*ptaille + 512-1)/512);
+
+		
+		if(strcmp(nom, chemin) == 0) {
+			
+			off_t position;
+			off_t endposition;
+
+			if((position=lseek(fd, -BLOCKSIZE , SEEK_CUR))==-1) break;
+			if((endposition=lseek(fd, 0 , SEEK_END))==-1) break;
+
+			unsigned int size= endposition - (position+BLOCKSIZE+filesize*BLOCKSIZE);
+			char cpy[size];
+
+			if(lseek(fd, position+BLOCKSIZE+filesize*BLOCKSIZE , SEEK_SET)==-1) break;
+			read(fd , cpy , size);
+
+			if(lseek(fd, position , SEEK_SET)==-1) break;
+			write(fd , cpy , size);
+
+			found=1;
+			break;
+			
+			
 		}
+		
+		
+		read(fd,header,BLOCKSIZE*filesize);
+	
+	}
+	
+	
+	close(fd);
+
 	
 	return 0;
 }
