1327378d9e3cf9cee899dd58ba38401685dc6c9f
BARRAULT Victor <victor.barrault@etu.univ-paris-diderot.fr>
2020-12-30 00:15:58 +0100

    test

--- code/test.c
+++ code/test.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <sys/wait.h>
+
 #include "ls.h"
 #include "cd.h"
 #include "pwd.h"
@@ -18,33 +20,30 @@ int main(int argc, char const *argv[]) {
 	generate_files();
 	int output = open("../test/test_out",  O_WRONLY + O_CREAT + O_TRUNC, S_IRWXU);
 	int input = open("../test/test_in",  O_RDONLY);
-	char in_str[64];
-	char out_str[64];
-	//write(output, "test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n", strlen("test_rep  ..  test_fic  test_exe2  .  test_exe1  test_lien  test_fifo  \n"));
-	//printf("o : %d, i : %d ", output, input);
 	int save = dup(1);
-	//printf("%d", save_out);
 	dup2(output, STDOUT_FILENO);
+
 	char *arg[2];
+	char *arg2[2];
 	arg[0] = "ls";
 	arg[1] = "tests";
-	ls(2,arg);
+	arg2[0] = "ls";
+	arg2[1] = "tests";
+	ls(2, arg);
+	ls(2, arg2);
 	close(output);
 	dup2(save, STDOUT_FILENO);
 	close(save);
 
 	int output2 = open("../test/test_out",  O_RDONLY);
-	read(output2, out_str, 63);
-	read(input, in_str, 63);
-	//printf(out_str);
-	//printf("%s", in_str);
-	if(strcmp(out_str, in_str) == 0){
-		printf("Les fichiers sont les mêmes");
-	}else{
-		printf("Les fichiers sont différents");
+	switch(fork()){
+		case -1:
+			exit(1);
+		case 0:
+			execlp("diff", "diff", "../test/test_out", "../test/test_in", NULL);
+		default:
+			wait(NULL);
 	}
-	close(input);
-	close(output2);
 	remove_files();
 	return 0;
 }
